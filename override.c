4be10860fb0f72603f696887f0999f724ce25ff8
Barış <39498732+Bariskizilkaya@users.noreply.github.com>
2025-01-26 02:02:06 +0300

    Create override.c

---
+++ override.c
@@ -0,0 +1,28 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <dlfcn.h>
+
+// Declare the original __libc_start_main function signature
+typedef int (*libc_start_main_t)(int (*main)(int, char**, char**), int, char**,
+                                 void (*init)(void), void (*fini)(void),
+                                 void (*rtld_fini)(void), void*);
+
+// Override __libc_start_main
+int __libc_start_main(int (*main)(int, char**, char**), int argc, char** argv,
+                      void (*init)(void), void (*fini)(void),
+                      void (*rtld_fini)(void), void* stack_end) {
+    printf("Custom __libc_start_main called!\n");
+
+    // Correctly call func1
+void (*func1)() = (void (*)())0x4000001151;
+    if (func1) {
+        printf("Calling func1 from the binary...\n");
+        func1();  // Call func1
+    } else {
+        printf("func1 not found!\n");
+    }
+
+    // Call the original __libc_start_main to proceed with normal execution
+    libc_start_main_t original_start_main = (libc_start_main_t)dlsym(RTLD_NEXT, "__libc_start_main");
+    return original_start_main(main, argc, argv, init, fini, rtld_fini, stack_end);
+}
